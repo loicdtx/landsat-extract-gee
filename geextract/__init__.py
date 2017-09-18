1486f1ac704ed67229f400aa70a2cd80331bfcea
Loïc Dutrieux <loic.dutrieux@gmail.com>
2017-09-18 15:10:19 -0500

    Function working, need to harmonize outputs structure

--- geextract/__init__.py
+++ geextract/__init__.py
@@ -15,7 +15,7 @@ ee.Initialize()
     # 3-
 
 def ts_extract(lon, lat, sensor, start, end = None, radius = None, bands = None,
-               stats = ['mean', 'median'], cfmask_val = 0):
+               stats = 'mean', cfmask_val = 0):
     """Perform a spatio temporal query to extract Landsat surface reflectance data
         from gee
 
@@ -33,7 +33,7 @@ def ts_extract(lon, lat, sensor, start, end = None, radius = None, bands = None,
         bands (list): List of Landsat band names. Optional, defaults to
             ['B1', 'B2', 'B3', 'B4', 'B5', 'B7'] in the case of LC8 sensor and to
             ['B1', 'B2', 'B3', 'B4', 'B5', 'B7'] otherwise.
-        stats (list): List of spatial aggregating functions to use. Only relevant
+        stats (str): Spatial aggregation function to use. Only relevant
             if a radius value is set.
         cfmask_val (int): Value of the cfmask band corresponding to valid pixels.
             Defaults to 0.
@@ -46,6 +46,7 @@ def ts_extract(lon, lat, sensor, start, end = None, radius = None, bands = None,
         """Cloud masking function"""
         invalid = image.select('cfmask').neq(cfmask_val)
         return image.mask(invalid.Not())
+
 
     # Check inputs
     if sensor not in ['LT4', 'LT5', 'LC8', 'LE7']:
@@ -61,9 +62,33 @@ def ts_extract(lon, lat, sensor, start, end = None, radius = None, bands = None,
             filterDate(start=start, opt_end=end)\
             .map(_mask_clouds)\
             .select(bands)
-    geometry = ee.Geometry.Point(lon, lat)
     if radius is not None:
-        geometry = geometry.buffer(radius)
+        # Define spatial aggregation function
+        if stats == 'mean':
+            fun = ee.Reducer.mean()
+        elif stats == 'median':
+            fun = ee.Reducer.median()
+        elif stats == 'max':
+            fun = ee.Reducer.max()
+        elif stats == 'min':
+            fun = ee.Reducer.min()
+        else:
+            raise ValueError('Unknown spatial aggregation function. Must be one of mean, median, max, or min')
+
+        geometry = ee.Geometry.Point(lon, lat).buffer(radius)
+        # Define function to map over imageCollection to perform spatial aggregation 
+        def _reduce_region(image):
+            """Spatial aggregation function for a single image and a polygon feature"""
+            stat_dict = image.reduceRegion(fun, geometry, 30);
+            # FEature needs to be rebuilt because the backend doesn't accept to map
+            # functions that return dictionaries
+            return ee.Feature(None, stat_dict)
+        out = landsat.filterBounds(geometry).map(_reduce_region)
+    else:
+        # Extraction with a point, no spatial aggregation, etc
+        geometry = ee.Geometry.Point(lon, lat)
+        out = landsat.filterBounds(geometry).getRegion(geometry, 30)
+    return out.getInfo()
 
     # col = ee.ImageCollection(product).\
     # select(bands).\
@@ -77,4 +102,14 @@ def ts_extract(lon, lat, sensor, start, end = None, radius = None, bands = None,
 # df.columns = data[0]
 # con = sqlite3.connect(db_src)
 # df.to_sql(name=table_name, con=con, if_exists='append')
+
+from pprint import pprint
+import datetime
+
+a = ts_extract(3, 45, 'LC8', start = '2010-01-01', end = '2017-09-18',
+               radius = 2000)
+pprint(a)
+
+b = ts_extract(3, 45, 'LC8', start = '2010-01-01', end = '2017-09-18')
+pprint(b)
 
