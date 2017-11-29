0f3a992f50005aacb03a4693a8246dc1cbc00a29
Loïc Dutrieux <loic.dutrieux@gmail.com>
2017-11-28 21:27:03 -0600

    Revert "Give up with travis"
    This reverts commit 2056adb8392d44d74a641ede6364b3787ab94a43.

---
+++ travis_setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# THis script is used to write the GEE API key (available as GEE_API_KEY environment variable
+# in the travis machine via the travis variable encription mechanism) to a file
+import os
+
+# Get key
+key = os.environ['GEE_API_KEY']
+
+# Build line
+line = '{"refresh_token": "%s"}' % key
+
+# Create directory 
+os.makedirs(os.path.expanduser('~/.config/earthengine/'))
+
+# Write line to file
+with open(os.path.expanduser('~/.config/earthengine/credentials', 'w')) as dst:
+   dst.write(line)
+
