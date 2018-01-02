bc491fcf4ad8709d100ffe77ecb880e6113f6295
Loïc Dutrieux <loic.dutrieux@gmail.com>
2018-01-01 18:21:00 -0600

    Wrote examples section of the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -35,8 +35,6 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
               'sphinx.ext.napoleon',
               'sphinxcontrib.programoutput',
-              'IPython.sphinxext.ipython_directive',
-              'IPython.sphinxext.ipython_console_highlighting',
               'matplotlib.sphinxext.only_directives',
               'matplotlib.sphinxext.plot_directive']
 
