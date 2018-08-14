efffd28fc6744287dedab9fca82427a1d415a398
Loïc Dutrieux <loic.dutrieux@gmail.com>
2018-08-15 00:25:42 +0200

    remove collecton argument in unit tests

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -6,7 +6,7 @@ class TestTsExtraction(unittest.TestCase):
     def test_point(self):
         a = ts_extract(lon=4.722111, lat=44.770928, sensor='LC8', start=datetime(2015,1,1),
                        end=datetime(2016, 6, 1), radius = None, feature = None, bands = None,
-                       stats = 'mean', collection = 1)
+                       stats = 'mean')
         self.assertTrue(len(a) > 2)
         self.assertTrue(isinstance(a[0], dict))
         self.assertEqual(set(a[0].keys()),
@@ -15,7 +15,7 @@ class TestTsExtraction(unittest.TestCase):
     def test_point_radius(self):
         a = ts_extract(lon=-3, lat=44.7, sensor='LC8', start=datetime(2015,1,1),
                        end=datetime(2016, 6, 1), radius = 300, feature = None, bands = None,
-                       stats = 'mean', collection = 1)
+                       stats = 'mean')
         self.assertTrue(len(a) > 2)
         self.assertTrue(isinstance(a[0], dict))
         self.assertEqual(set(a[0].keys()),
@@ -24,7 +24,7 @@ class TestTsExtraction(unittest.TestCase):
     def test_point_radius_tm(self):
         a = ts_extract(lon=-3, lat=44.7, sensor='LT5', start=datetime(1999,1,1),
                        end=datetime(2005, 6, 1), radius = 300, feature = None, bands = None,
-                       stats = 'median', collection = 1)
+                       stats = 'median')
         self.assertTrue(len(a) > 2)
         self.assertTrue(isinstance(a[0], dict))
         self.assertEqual(set(a[0].keys()),
@@ -38,8 +38,7 @@ class TestTsExtraction(unittest.TestCase):
                   'start': datetime(1999, 1, 1),
                   'end': datetime(2000, 6, 1),
                   'radius': 300,
-                  'stats': 'max',
-                  'collection': 2}
+                  'stats': 'max'}
         self.assertRaises(ValueError, ts_extract, **kwargs_0)
         # sensor does not exist
         kwargs_1 = {'lon': -3,
@@ -48,8 +47,7 @@ class TestTsExtraction(unittest.TestCase):
                   'start': datetime(1999, 1, 1),
                   'end': datetime(2000, 6, 1),
                   'radius': 300,
-                  'stats': 'max',
-                  'collection': 1}
+                  'stats': 'max'}
         self.assertRaises(ValueError, ts_extract, **kwargs_1)
         # aggregation method does not exist
         kwargs_2 = {'lon': -3,
@@ -58,6 +56,5 @@ class TestTsExtraction(unittest.TestCase):
                   'start': datetime(1999, 1, 1),
                   'end': datetime(2000, 6, 1),
                   'radius': 300,
-                  'stats': 'mode',
-                  'collection': 1}
+                  'stats': 'mode'}
         self.assertRaises(ValueError, ts_extract, **kwargs_2)
